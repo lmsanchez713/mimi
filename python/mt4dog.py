4d0064176c411addfde9879b69fd45622955c4fc
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2021-07-31 21:28:15 -0300

    Watchdog MT4 pronto para receber código DERIV

--- python/mt4dog.py
+++ python/mt4dog.py
@@ -3,7 +3,19 @@ import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
-#from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler
+
+class CACHORRO_VIGIA(FileSystemEventHandler):
+    def on_any_event(self, evento):
+
+        # Aqui se insere o código a ser executado quando houver um evento de arquivo da pasta
+        # (código da DERIV)
+        print("TIPO:")
+        print(type(evento))
+        print("DIR:")
+        print(dir(evento))
+        print("VARS:")
+        print(vars(evento))
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
@@ -11,8 +23,10 @@ if __name__ == "__main__":
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     event_handler = LoggingEventHandler()
+    CACHORRAO = CACHORRO_VIGIA()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    # observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(CACHORRAO, path, recursive=True)
     observer.start()
     try:
         while True:
