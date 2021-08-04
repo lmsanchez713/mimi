78dce18a8242b30db3b228fd0b199e98ce85225d
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2021-08-03 23:00:00 -0300

    Sentinela MT4 pronto para primeiros testes
    O sentinela está identificando alterações nos arquivos CSV e mostrando na tela as novas linhas

--- python/mt4dog.py
+++ python/mt4dog.py
@@ -10,15 +10,31 @@ import shutil
 
 class sentinela_de_arquivos(FileSystemEventHandler):
     def on_created(self, evento):
-        print("ARQUIVO CRIADO: " + evento.src_path)
-        # print(vars(evento))
+        print("Arquivo criado: " + evento.src_path)
+        split_path = evento.src_path.split('\\')
+        target_path = os.path.join(pasta_copias, split_path[-1])
+        try:
+            shutil.copyfile(evento.src_path, target_path)
+            print("Copiando para " + target_path)
+        except Exception as e:
+            print('Falha ao copiar %s. Motivo: %s' % (target_path, e))
     def on_modified(self, evento):
-        print("ARQUIVO MODIFICADO: " + evento.src_path)
+        print("Arquivo modificado: " + evento.src_path)
+        print(vars(evento))
+        split_path = evento.src_path.split('\\')
+        target_path = os.path.join(pasta_copias, split_path[-1])
+        print("Comparando com o arquivo " + target_path + "...")
+        with open(evento.src_path, 'r') as file1:
+            with open(target_path, 'r') as file2:
+                same = set(file1).difference(file2)
+                print("Linhas diferentes:")
+                print(same)
 
 if __name__ == "__main__":
     print("Mimi Watchdog v0.1")
     
-    pasta_csv = sys.argv[1] if len(sys.argv) > 1 else 'C:\\mimi\\testes\\csv'
+    # pasta_csv = sys.argv[1] if len(sys.argv) > 1 else 'C:\\mimi\\testes\\csv'
+    pasta_csv = sys.argv[1] if len(sys.argv) > 1 else 'C:\Users\marro\AppData\Roaming\MetaQuotes\Terminal\9D15457EC01AD10E06A932AAC616DC32\MQL4\Files'
     pasta_copias = str(pathlib.Path(__file__).parent.resolve()) + "\copias"
     if not os.path.exists(pasta_copias):
         os.mkdir(pasta_copias)
