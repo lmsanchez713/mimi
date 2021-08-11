cc3657c2ab5c109f4dbdb23a7e10e13d4f411c0c
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2021-08-11 17:18:56 -0300

    mt4dog.py em sua estrutura final (watchdog e websockets async)

--- python/mt4dog.py
+++ python/mt4dog.py
@@ -27,6 +27,14 @@ async def autenticar_deriv(soquete, token):
     resposta = await enviar_deriv(soquete, requisicao)
     return resposta
 
+def processar_sinal_retorno_v2(sinal):
+    colunas = sinal.split(",")
+    if len(colunas) == 6:
+        print("Sinal salvo")
+        print("timestamp:\t" + colunas[0] + "\tativo:\t" + colunas[1] + "\tacao:\t" + colunas[2])
+        print("expiracao:\t" + colunas[3] + "\tgale:\t" + colunas[4] + "\tvalor:\t" + colunas[5])
+        #
+
 class sentinela_de_arquivos(FileSystemEventHandler):
     def on_created(self, evento):
         # print("Arquivo criado: " + evento.src_path)
@@ -50,12 +58,7 @@ class sentinela_de_arquivos(FileSystemEventHandler):
                 linha = file1.readline()
                 # print("Linhas novas:")
                 while len(linha):
-                    colunas = linha.split(",")
-                    if len(colunas) == 6:
-                        print("Sinal salvo")
-                        print("timestamp:\t" + colunas[0] + "\tativo:\t" + colunas[1] + "\tacao:\t" + colunas[2])
-                        print("expiracao:\t" + colunas[3] + "\tgale:\t" + colunas[4] + "\tvalor:\t" + colunas[5])
-                        #
+                    processar_sinal_retorno_v2(linha)
                     linha = file1.readline()
                 # print(os.path.getsize(evento.src_path))
                 # print(file2.read())
@@ -87,76 +90,51 @@ async def funcao_principal():
             print("Falha na autenticação. Saindo do programa...")
 
         while rodando:
-            name = input("Digite 'exit' para sair")
+            name = input("Digite 'exit' para sair\n")
             if name == 'exit':
                 rodando = False
+            else:
+                processar_sinal_retorno_v2(name)
 
 if __name__ == "__main__":
-    asyncio.run(funcao_principal())
-
-    # asyncio.get_event_loop().run_forever()
-
-    # soquete = websockets.connect("wss://ws.binaryws.com/websockets/v3?app_id=1089", ssl=True)
-
-    # req = """{"authorize":"HVumMdEbE5TdVUP"}"""
-
-    # asyncio.get_event_loop().run_until_complete(auth_deriv("HVumMdEbE5TdVUP"))
-
-    # async def hello():
-    #     uri = "wss://ws.binaryws.com/websockets/v3?app_id=1089"
-    #     async with websockets.connect(
-    #         uri, ssl=True
-    #     ) as websocket:
-    #         req = """{"authorize":"HVumMdEbE5TdVUP"}"""
-
-    #         await websocket.send(req)
-    #         print(f"> {req}")
-
-    #         greeting = await websocket.recv()
-    #         print(f"< {greeting}")
-
-    # asyncio.get_event_loop().run_until_complete(hello())
-
-
     
-    # pasta_csv = sys.argv[1] if len(sys.argv) > 1 else 'C:\\mimi\\testes\\csv'
-    # # pasta_csv = sys.argv[1] if len(sys.argv) > 1 else 'C:\\Users\\marro\\AppData\\Roaming\\MetaQuotes\\Terminal\\9D15457EC01AD10E06A932AAC616DC32\\MQL4\\Files'
-    # pasta_copias = str(pathlib.Path(__file__).parent.resolve()) + "\copias"
-    # if not os.path.exists(pasta_copias):
-    #     os.mkdir(pasta_copias)
+    pasta_csv = sys.argv[1] if len(sys.argv) > 1 else 'C:\\mimi\\testes\\csv'
+    # pasta_csv = sys.argv[1] if len(sys.argv) > 1 else 'C:\\Users\\marro\\AppData\\Roaming\\MetaQuotes\\Terminal\\9D15457EC01AD10E06A932AAC616DC32\\MQL4\\Files'
+    pasta_copias = str(pathlib.Path(__file__).parent.resolve()) + "\copias"
+    if not os.path.exists(pasta_copias):
+        os.mkdir(pasta_copias)
     
-    # print("Pasta CSV: " + pasta_csv)
-    # print("Pasta copias: " + pasta_copias)
+    print("Pasta CSV: " + pasta_csv)
+    print("Pasta copias: " + pasta_copias)
 
-    # print("Esvaziando pasta copias...")
-    # for filename in os.listdir(pasta_copias):
-    #     file_path = os.path.join(pasta_copias, filename)
-    #     try:
-    #         if os.path.isfile(file_path) or os.path.islink(file_path):
-    #             os.unlink(file_path)
-    #         elif os.path.isdir(file_path):
-    #             shutil.rmtree(file_path)
-    #         print("Apagando " + file_path)
-    #     except Exception as e:
-    #         print('Falha ao apagar %s. Motivo: %s' % (file_path, e))
+    print("Esvaziando pasta copias...")
+    for filename in os.listdir(pasta_copias):
+        file_path = os.path.join(pasta_copias, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+            print("Apagando " + file_path)
+        except Exception as e:
+            print('Falha ao apagar %s. Motivo: %s' % (file_path, e))
 
-    # print("Copiando pasta csv...")
-    # for filename in os.listdir(pasta_csv):
-    #     file_path = os.path.join(pasta_csv, filename)
-    #     copy_path = os.path.join(pasta_copias, filename)
-    #     try:
-    #         shutil.copyfile(file_path, copy_path)
-    #         print("Copiando " + file_path)
-    #     except Exception as e:
-    #         print('Falha ao copiar %s. Motivo: %s' % (file_path, e))
+    print("Copiando pasta csv...")
+    for filename in os.listdir(pasta_csv):
+        file_path = os.path.join(pasta_csv, filename)
+        copy_path = os.path.join(pasta_copias, filename)
+        try:
+            shutil.copyfile(file_path, copy_path)
+            print("Copiando " + file_path)
+        except Exception as e:
+            print('Falha ao copiar %s. Motivo: %s' % (file_path, e))
 
-    # sentinela_mt4 = sentinela_de_arquivos()
-    # observer = Observer()
-    # observer.schedule(sentinela_mt4, pasta_csv, recursive=True)
-    # observer.start()
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # finally:
-    #     observer.stop()
-    #     observer.join()
+    sentinela_mt4 = sentinela_de_arquivos()
+    observer = Observer()
+    observer.schedule(sentinela_mt4, pasta_csv, recursive=True)
+    observer.start()
+    try:
+        asyncio.run(funcao_principal())
+    finally:
+        observer.stop()
+        observer.join()
